7e48713cedd6e8bd50241739d4643450c9c9ebc6
sandyelite11-pixel <sandyelite11@gmail.com>
2026-09-19 16:24:19 +0500

    Delete logout.js

--- logout.js
+++
@@ -1,2 +0,0 @@
-const {clearCookie,json}=require('./_lib');
-module.exports=async function(req,res){if(req.method!=='POST')return json(res,405,{error:'Method not allowed'});return json(res,200,{ok:true},{'Set-Cookie':clearCookie()})}
